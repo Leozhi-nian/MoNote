6b3690d9c6569d15019846b8ed67147b9c2edf88
leozhi <leozhi819@gmail.com>
2020-09-14 16:55:22 +0800

    init
    创建项目

---
+++ app/src/main/java/com/leozhi/monote/MainActivity.java
@@ -0,0 +1,17 @@
+package com.leozhi.monote;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author leozhi
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
